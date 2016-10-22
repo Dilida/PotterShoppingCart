b9541ac13785a2d3f1b68f2938e2ba2fbd148be0
Dilida <Dilida@DD>
2016-10-23 00:42:33 +0800

    checkOut_EP1_was_100_failed

---
+++ PotterShoppingCart.Tests/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PotterShoppingCart.Tests
+{
+    internal class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+        }
+
+        internal object orderFee()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Add(BookData bookData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PotterShoppingCart.Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass]
+    public class bookShoppintCartTest
+    {
+        [TestMethod]
+        public void checkOut_EP1_was_100()
+        {
+            var bookData = new BookData() { EP=1, Price=100 };
+            var shoppingCart = new ShoppingCart();
+
+            shoppingCart.Add(bookData);
+            var expected = 100;
+            var actual = shoppingCart.orderFee();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
+
+namespace PotterShoppingCart.Tests
+{
+    public class BookData
+    {
+        public BookData()
+        {
+        }
+
+        public int EP { get; set;}
+        public int Price { get; set; }
+    }
+}
