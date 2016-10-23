c0900a23a4a0100e54cf37a77fd5e03cfe92a53b
Dilida <Dilida@DD>
2016-10-23 18:18:36 +0800

    checkOut_fourBook_was_80_discounted_successed

--- PotterShoppingCart.Tests/ShoppingCart.cs
+++ PotterShoppingCart.Tests/ShoppingCart.cs
@@ -27,6 +27,7 @@ namespace PotterShoppingCart.Tests
             {
                 case 2 : return 0.95;
                 case 3 : return 0.9;
+                case 4 : return 0.8;
                 default: return 1;
                    
             }
