70b907f1f50e5e31724607d5b5f63620640c241d
Dilida <Dilida@DD>
2016-10-23 18:17:24 +0800

    checkOut_fourBook_was_80_discounted_failed

--- PotterShoppingCart.Tests/UnitTest1.cs
+++ PotterShoppingCart.Tests/UnitTest1.cs
@@ -60,6 +60,26 @@ namespace PotterShoppingCart.Tests
             Assert.AreEqual(expected, actual);
 
         }
+
+        [TestMethod]
+        public void checkOut_fourBook_was_80_discounted()
+        {
+            var booksData = new List<BookData>{
+                new BookData { EP =1, Price =100, Quantity = 1},
+                new BookData { EP =2, Price =100, Quantity = 1},
+                new BookData { EP =3, Price =100, Quantity = 1},
+                new BookData { EP =4, Price =100, Quantity = 1 }
+            };
+            var shoppingCart = new ShoppingCart();
+
+            shoppingCart.Add(booksData);
+            var expected = 320;
+            var actual = shoppingCart.orderFee();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
     }
 }
 
